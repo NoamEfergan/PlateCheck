c91b2fec033a771000586ba904efef71c5af623e
Noam Efergan <efergann@gmail.com>
2023-03-07 13:45:43 +0000

    Build initial VC. nothing connected yet.

---
+++ PlateCheck/Models/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PlateCheck
+//
+//  Created by Noam Efergan on 07/03/2023.
+//
+
+import UIKit
+
+enum AppColours {
+    static let background: UIColor? = .init(hexString: "#F9E29A")
+    static let mainColor: UIColor? = .init(hexString: "#363635")
+    static let secondaryColor: UIColor? = .init(hexString: "#626163")
+}
